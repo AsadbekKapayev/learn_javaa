d014e905a15d57ca8cb78d112ad86944fc0f9218
akapayev <akapayev@greet-go.com>
2023-11-29 19:07:44 +0600

    feat: hash map added

---
+++ src/com/company/_4_collection/_new/HashMapEx.java
@@ -0,0 +1,17 @@
+package com.company._4_collection._new;
+
+import java.util.HashMap;
+
+public class HashMapEx {
+
+  //region hashmap ex1
+  public static void main(String[] args) {
+    HashMap<String, String> hashMap = new HashMap<>();
+
+    hashMap.put("akMxQs3M1w", "value");
+
+    System.out.println(hashMap);
+  }
+  //endregion
+
+}
