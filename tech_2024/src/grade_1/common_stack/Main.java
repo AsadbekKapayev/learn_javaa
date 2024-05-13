b500bcb2045bff05542b536fc7db9fb11e66b9ee
akapayev <akapayev@greet-go.com>
2024-05-13 20:13:30 +0500

    feat: common stack -> 1

---
+++ tech_2024/src/grade_1/common_stack/Main.java
@@ -0,0 +1,37 @@
+package grade_1.common_stack;
+
+public class Main {
+
+  public static void main(String[] args) {
+  }
+
+  //region <1> Singleton Eager Initialization
+  static class Singleton {
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+      return instance;
+    }
+  }
+  //endregion
+
+  //region <2> Singleton Lazy Initialization
+  static public class Singleton2 {
+    private static Singleton2 instance;
+
+    private Singleton2() {
+    }
+
+    public static synchronized Singleton2 getInstance() {
+      if (instance == null) {
+        instance = new Singleton2();
+      }
+      return instance;
+    }
+  }
+  //endregion
+
+}
